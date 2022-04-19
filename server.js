dd514d866457be7f2f6cf6237bb4a2d1b5ba500c
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-04-19 16:11:20 -0400

    Max Aggregation and Fix URL Query Parsing

--- server.js
+++ server.js
@@ -6,22 +6,82 @@ var MongoClient = require('mongodb').MongoClient;
 
 var mongo_url = "mongodb://localhost:27017/";
 
+static_aggregate = [
+  {
+    '$match': null,
+  },
+  {
+    '$sort': {
+      'Value': -1
+    }
+  }, {
+    '$group': {
+      '_id': '$Sensor-id', 
+      'Value': {
+        '$first': '$Value'
+      },
+      'Timestamp': {
+        '$first': '$Timestamp'
+      },
+      'Sensor-id': {
+        '$first': '$Sensor-id'
+      }
+    }
+  }
+]
+mobile_aggregate = [
+  {
+    '$match': null,
+  },
+  {
+    '$sort': {
+      'Value': -1
+    }
+  }, {
+    '$group': {
+      '_id': {'$concat': [
+        {'$toString': '$Lat'},
+        ',',
+        {'$toString': '$Long'}
+      ]},
+      'Value': {
+        '$first': '$Value'
+      },
+      'Timestamp': {
+        '$first': '$Timestamp'
+      },
+      'Sensor-id': {
+        '$first': '$Sensor-id'
+      },
+      ' User-id': {
+        '$first': '$ User-id'
+      },
+      'Lat': {
+        '$first': '$Lat'
+      },
+      'Long': {
+        '$first': '$Long'
+      }
+    }
+  }
+]
+
 http.createServer(function (req, res) {
   pathname = url.parse(req.url, true).pathname;
   if (pathname == "/data")
   {
     // parse query
     var url_query = url.parse(req.url, true).query;
-    url_query.start_date = url_query.start_date == null ?
+    url_query.start_date = url_query.start_date != null ?
       new Date(url_query.start_date)
       : new Date("2020-04-06T04:00:10");
-    url_query.end_date = url_query.end_date == null?
+    url_query.end_date = url_query.end_date != null?
       new Date(url_query.end_date)
       : new Date("2020-04-06T04:00:30");
-    url_query.min_val = url_query.min_val == null ?
+    url_query.min_val = url_query.min_val != null ?
       parseFloat(url_query.min_val)
       : 0.0;
-    url_query.max_val = url_query.max_val == null ?
+    url_query.max_val = url_query.max_val != null ?
       parseFloat(url_query.max_val)
       : 10000.0;
     if (url_query.tag != null)
@@ -44,10 +104,13 @@ http.createServer(function (req, res) {
         static_query["Sensor-id"] = url_query.sensor_id;
       else if (url_query.user_id != null)
         mobile_query["Sensor-id"] = url_query.sensor_id;
+      // add query to aggregate commands
+      static_aggregate[0]['$match'] = static_query;
+      mobile_aggregate[0]['$match'] = mobile_query;
       // request queries to get them running, need to await variables to get results
       raw_static_locs = dbo.collection("StaticSensorLocations").find().toArray();
-      raw_static_data = dbo.collection("StaticSensorReadings").find(static_query).toArray();
-      raw_mobile_data = dbo.collection("MobileSensorReadings").find(mobile_query).toArray();
+      raw_static_data = dbo.collection("StaticSensorReadings").aggregate(static_aggregate).toArray();
+      raw_mobile_data = dbo.collection("MobileSensorReadings").aggregate(mobile_aggregate).toArray();
       // process static data
       static_locs = Array.from(await raw_static_locs,
         (d) => { return {
